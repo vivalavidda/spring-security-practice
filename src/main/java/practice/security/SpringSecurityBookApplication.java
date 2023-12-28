0d4770d49a6d71c5503cc861c1023fce5a0fd95d
yun <vivalavida.devv@gmail.com>
2023-12-28 23:55:27 +0900

    init: spring 프로젝트 생성

---
+++ src/main/java/practice/security/SpringSecurityBookApplication.java
@@ -0,0 +1,12 @@
+package practice.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBookApplication.class, args);
+    }
+}
